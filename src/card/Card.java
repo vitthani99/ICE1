05b90786753c95fff4f8441748f10e8f5160ccaf
vitthani99 <133705134+vitthani99@users.noreply.github.com>
2023-05-30 10:00:26 -0400

    Update Card.java

--- src/card/Card.java
+++ src/card/Card.java
@@ -13,38 +13,38 @@ package card;
  * add your name as a modifier.
  * @author srinivsi
  */
-public class Card {
+public class luckyCard {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+   private String suit_1; //clubs, spades, diamonds, hearts
+   private int value_1;//1-13
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
     /**
      * @return the suit
      */
-    public String getSuit() {
-        return suit;
+    public String getSuit_1() {
+        return suit_1;
     }
 
     /**
      * @param suit the suit to set
      */
-    public void setSuit(String suit) {
-        this.suit = suit;
+    public void setSuit_1(String suit_1) {
+        this.suit_1 = suit_1;
     }
 
     /**
      * @return the value
      */
-    public int getValue() {
-        return value;
+    public int getValue_1() {
+        return value_1;
     }
 
     /**
      * @param value the value to set
      */
-    public void setValue(int value) {
-        this.value = value;
+    public void setValue_1(int value_1) {
+        this.value_1 = value_1;
     }
    
    
